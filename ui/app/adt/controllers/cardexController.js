a54d5a18336c4ddb6c281188715f8cda9acd53ad
00euclid <felixsakhya666@gmail.com>
2021-03-10 17:06:17 +0545

    Minor Changes

--- ui/app/adt/controllers/cardexController.js
+++ ui/app/adt/controllers/cardexController.js
@@ -2,8 +2,11 @@
 
 angular.module('bahmni.adt')
     .controller('CardexController', [
-        '$scope', '$rootScope', '$stateParams', 'spinner',
-        ($scope, $rootScope, $stateParams, spinner) => {
+        '$scope', '$rootScope', '$stateParams', 'spinner', 'drugService', 'contextChangeHandler', 
+        '$q', 'ngDialog', '$window', '$timeout', '$translate',
+        ($scope, $rootScope, $stateParams, spinner, drugService, contextChangeHandler, 
+        $q, ngDialog, $window, $timeout, $translate) => {
+            
             const init = () => {
                 console.log("Success!");
             };
